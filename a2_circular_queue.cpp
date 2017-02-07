74b0d17ab7278a9c3d7a73b4f4cc410770f05759
Jimmy Xu <xujimmyy@gmail.com>
2017-02-07 15:13:41 -0500

    20/20 test cases

--- a2_circular_queue.cpp
+++ a2_circular_queue.cpp
@@ -1,46 +1,86 @@
 #include <iostream>
 #include "a2_circular_queue.hpp"
 
+using namespace std;
+
 typedef CircularQueue::QueueItem QueueItem;
 const QueueItem CircularQueue::EMPTY_QUEUE = -999;
 
 CircularQueue::CircularQueue()
 {
+	capacity_=16;
+	items_ = new QueueItem[16];
+	size_=0;
+	head_=0;
+	tail_=0;
 }
 
-CircularQueue::CircularQueue(unsigned int capacity)
+CircularQueue::CircularQueue(unsigned int cap)
 {
+	capacity_=cap;
+	items_ = new QueueItem[cap];
+	size_=0;
+	head_=0;
+	tail_=0;
 }
 
 CircularQueue::~CircularQueue()
 {
+	delete items_;
 }
 
 bool CircularQueue::empty() const
 {    
+	return (size_==0);
 }
 
 bool CircularQueue::full() const
 {
+	return (size_==capacity_);
 }
 
 int CircularQueue::size() const
 {  
+	return size_;
 }
 
 bool CircularQueue::enqueue(QueueItem value)
 {
+	if (full()){
+		return false;
+	}
+	items_[tail_]=value;
+	tail_++;
+	tail_%=(capacity_);
+	size_++;
+	return true;
 }
 
 QueueItem CircularQueue::dequeue()
 {
+	if (empty()){
+		return EMPTY_QUEUE;
+	}
+	QueueItem temp= items_[head_];
+	head_++;
+	head_%=(capacity_);
+	size_--;
+	return temp;
 }
 
 QueueItem CircularQueue::peek() const
 {
+	if (empty()){
+		return EMPTY_QUEUE;
+	}
+	return items_[head_];
 }
 
 void CircularQueue::print() const
 {
+	for(int i=head_;i<=head_+size_;i++){
+		cout<<items_[i%size_]<<" ";
+	}
+	cout<<endl;
 }
 
--- a2_dynamic_stack.cpp
+++ a2_dynamic_stack.cpp
@@ -1,42 +1,87 @@
 #include <iostream>
 #include "a2_dynamic_stack.hpp"
 
+using namespace std;
+
 typedef DynamicStack::StackItem StackItem;  // for simplicity
 const StackItem DynamicStack::EMPTY_STACK = -999;
 
 DynamicStack::DynamicStack()
 {
+	init_capacity_=16;
+	capacity_=16;
+	items_ = new StackItem[16];
+	size_=0;
 }
 
-DynamicStack::DynamicStack(unsigned int size_)
+DynamicStack::DynamicStack(unsigned int cap)
 {
+	init_capacity_=cap;
+	capacity_=cap;
+	items_ = new StackItem[cap];
+	size_=0;
 }
 
 DynamicStack::~DynamicStack()
 {
+	delete items_;
 }
 
 bool DynamicStack::empty() const
 {
+	return (size_==0);
 }
 
 int DynamicStack::size() const
 {
+	return size_;
 }
 
 void DynamicStack::push(StackItem value)
 {
+	if (size_==capacity_){
+		StackItem* temp = new StackItem [capacity_*2];
+		for(int i=0;i<capacity_;i++){
+			temp[i]=items_[i];
+		}
+		capacity_*=2;
+		delete items_;
+		items_=temp; 
+	}
+	items_[size_]=value;
+	size_++;
 }
 
 StackItem DynamicStack::pop()
 {
+	if (size_==0){
+		return EMPTY_STACK;
+	}else if (size_<=capacity_/4 && capacity_/2>=init_capacity_){
+		StackItem* temp = new StackItem [capacity_/2];
+		for(int i=0;i<size_;i++){
+			temp[i]=items_[i];
+		}
+		capacity_/=2;
+		delete items_;
+		items_=temp;
+	}
+	size_--;
+	return items_[size_];
 }
 
 StackItem DynamicStack::peek() const
 {
+	if (size_==0){
+		return EMPTY_STACK;
+	}
+	return items_[size_-1];
 }
 
 void DynamicStack::print() const
 {
+	for(int i=size_-1;i>=0;i--){
+		cout<<items_[i]<<" ";
+	}
+	cout<<endl;
 }
 
